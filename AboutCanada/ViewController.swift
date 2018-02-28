565c027732235708b08168da6bf1e8c237933b60
RadX <ralobba@gmail.com>
2018-02-28 23:44:30 +1100

    Initial Commit

---
+++ AboutCanada/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AboutCanada
+//
+//  Created by Rex Jason Alobba on 28/2/18.
+//  Copyright © 2018 Rex Jason Alobba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
