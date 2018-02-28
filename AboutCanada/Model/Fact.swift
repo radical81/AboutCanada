99339b1d5186b765610b10d55c9da8bdca1e1b94
RadX <ralobba@gmail.com>
2018-02-28 23:59:22 +1100

    Create data structure representing a fact about Canada

---
+++ AboutCanada/Model/Fact.swift
@@ -0,0 +1,15 @@
+//
+//  Fact.swift
+//  AboutCanada
+//
+//  Created by Rex Jason Alobba on 28/2/18.
+//  Copyright © 2018 Rex Jason Alobba. All rights reserved.
+//
+
+import Foundation
+
+struct Fact: Codable {
+    var title: String?
+    var description: String?
+    var imageHref: String?
+}
