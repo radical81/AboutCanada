9e9c6a90264fe00ff5cba5e03d1503fa7a5125c5
RadX <ralobba@gmail.com>
2018-03-01 23:16:38 +1100

    Create custom table view cell

---
+++ AboutCanada/Controller/FactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FactTableViewCell.swift
+//  AboutCanada
+//
+//  Created by Rex Jason Alobba on 1/3/18.
+//  Copyright © 2018 Rex Jason Alobba. All rights reserved.
+//
+
+import UIKit
+
+class FactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
